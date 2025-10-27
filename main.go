57376bd44a475c33d79196202e5cbbb11c482500
JuanSebastianPerniola <juanperniolarugby@gmail.com>
2025-10-27 16:36:47 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+// Function a set of a instruccion a name is optinal
+// Example:
+//
+//	set name value
+//	set value
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
