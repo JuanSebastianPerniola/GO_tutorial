402c5c804762679641174e23079db50fbcec2441
JuanSebastianPerniola <juanperniolarugby@gmail.com>
2025-11-03 08:43:45 +0100

    context propagation

--- ContextPropagation/ContextProp.go
+++ ContextPropagation/ContextProp.go
@@ -1,20 +1,45 @@
-// ConteztPropagation
+// Context Propagation
+// set a time out of 2 seconds
+// launch 3 workers each simulating a work of long running tasks with random  duration set duration 1-4 s
+// worker -> listen to ctx.Done() to if is cancelled
+// Print a mssg
+// Wait for all go routine to finish before existin the main function
+
 package main
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
-func main() {
-	go thread(1)
-	go thread(2)
-	fmt.Scanln()
-}
+// We set a variable context in args so can be propagated
+// For better readiblity ill use context instead of ctx
+func worker(context context.Context, id int, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-func thread(id int) {
-	for i := 1; i < 10; i++ {
-		fmt.Print("Thread", id, " : ", i)
-		time.Sleep(100 * time.Millisecond)
+	fmt.Printf("Worker %d: Starting work\n", id)
+	workTime := time.Duration(rand.Intn(4)+1) * time.Second
+
+	select {
+	case <-time.After(workTime):
+		fmt.Printf("Worker %d: Completed work in %v\n", id, workTime)
+	case <-context.Done():
+		fmt.Printf("Worker %d: cancelled %v\n", id, context.Err())
 	}
+}
+func main() {
+	rand.New(rand.NewSource(time.Now().UnixNano())) // Diferentes latencias de red o carga del sistema
+	context, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	var waitgroup sync.WaitGroup
+	for i := 1; i <= 3; i++ {
+		waitgroup.Add(1)
+		go worker(context, i, &waitgroup)
+	}
+	waitgroup.Wait()
+	fmt.Println("All workers finished")
 }
